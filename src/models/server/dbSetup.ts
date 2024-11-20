1194ba236da6751eab046f978014f704b94b5e9b
Kristian <10221425@ucn.dk>
2024-11-20 09:11:08 +0100

    Data modelling in Appwrite with Node SDK

---
+++ src/models/server/dbSetup.ts
@@ -0,0 +1,34 @@
+// Bring all the models together and get or create the tables in the database
+import { db } from "../name";
+import createAnswerCollection from "./answer.collection";
+import createCommentCollection from "./comment.collection";
+import createQuestionCollection from "./question.collection";
+import createVoteCollection from "./vote.collection";
+
+import { databases } from "./config";
+
+export default async function getOrCreateDB(){
+  try {
+    await databases.get(db)
+    console.log("Database connection")
+  } catch (error) {
+    console.log("Database not found, creating database", error)
+    try {
+      await databases.create(db, db)
+      console.log("database created")
+      //create collections
+      await Promise.all([
+        createQuestionCollection(),
+        createAnswerCollection(),
+        createCommentCollection(),
+        createVoteCollection(),
+      ])
+      console.log("Collection created")
+      console.log("Database connected")
+    } catch (error) {
+      console.log("Error creating databases or collection", error)
+    }
+  }
+
+  return databases
+}
